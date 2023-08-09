4d3fdc4e07c5d877551de41caa1b905e30b47537
Marco MOSCARDINI <marco@hozana.org>
2023-08-09 10:29:41 +0200

    remove sonarqube token

--- sonar.sh
+++ sonar.sh
@@ -5,4 +5,4 @@ docker run \
   sonarsource/sonar-scanner-cli \
   -Dsonar.projectKey=hozana_php-api_AYnPNZCiK8WAZS0kN_A5 \
   -Dsonar.scm.provider=git \
-  -Dsonar.token=sqp_a6dbb71b77c86d5979d0bd6e1cf31b00b9e8bdb3 \
+  -Dsonar.token= \
