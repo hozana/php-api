d2f046c7e805c002732962aab2427c7b6079c6a1
Marco MOSCARDINI <marco@hozana.org>
2023-08-14 17:49:59 +0200

    remove sonarqube leaked token

--- sonar.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-  --rm \
-  -e SONAR_HOST_URL="https://sonarqube.hozana.org" \
-  -v "./:/usr/src" \
-  sonarsource/sonar-scanner-cli \
-  -Dsonar.projectKey=hozana_php-api_AYnPNZCiK8WAZS0kN_A5 \
-  -Dsonar.scm.provider=git \
-  -Dsonar.token= \
