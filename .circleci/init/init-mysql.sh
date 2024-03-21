82be1b45bf463ac82c2ad57ec5b06e9751544d34
Marco MOSCARDINI <mmoscardini@gac-technology.com>
2024-03-21 19:26:52 +0100

    remove sql files for crm

--- .circleci/init/init-mysql.sh
+++ .circleci/init/init-mysql.sh
@@ -6,4 +6,3 @@ cp hozana_org.sql hozana_e2e.sql && sed -i 's/USE `hozana`;/USE `hozana_e2e`;/g'
 mysql -h 127.0.0.1 -u hozana -phozana hozana_e2e < hozana_e2e.sql && rm hozana_e2e.sql
 mysql -h 127.0.0.1 -u root -proot -e "create database if not exists hozana_crm"
 mysql -h 127.0.0.1 -u root -proot -e "GRANT ALL ON hozana_crm.* TO 'hozana'@'%' WITH GRANT OPTION;"
-mysql -h 127.0.0.1 -u hozana -phozana hozana_crm < hozana_crm.sql
